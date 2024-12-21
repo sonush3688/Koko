3fbf6095dd1bf1a06d32eeb9030b7de8b34f3199
sonush3688 <manavsh360@outlook.com>
2024-12-21 19:26:36 +0530

    Add files via upload

---
+++ rajaji.py
@@ -0,0 +1,16 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7343295464:AAEM7vk5K3cNXAywZC_Q11wmMzMu4gk09PU")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Shsonu")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6218253783")
+
+
+
+
+  
